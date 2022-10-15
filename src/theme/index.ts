3d8e0fc6af819426701b532bdf27eac9758fa954
alvdelci <rafithydev@gmail.com>
2022-10-15 10:45:23 -0300

    feat: Initializing React Native project

---
+++ src/theme/index.ts
@@ -0,0 +1,20 @@
+export const THEME = {
+  COLORS: {
+    BACKGROUND: "#171717",
+    GREEN: "#27AE60",
+    GRAY1: "#828282",
+    GRAY2: "#535353",
+  },
+  FONT_SIZE: {
+    TITLE: 48,
+    LG: 36,
+    MD: 18,
+    SM: 14,
+  },
+  FONT_FAMILY: {
+    REGULAR: "",
+    SEMI_BOLD: "",
+    BOLD: "",
+    BLACK: "",
+  },
+};

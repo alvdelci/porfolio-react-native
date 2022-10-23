01e22cff66f89864e8ab5214885581e415ca80b2
alvdelci <rafithydev@gmail.com>
2022-10-23 00:44:35 -0300

    feat: Add about, what i do and main skills

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,88 @@
-import { View, SafeAreaView, Text } from 'react-native'
-import React from 'react'
-import { styles } from './styles'
-import Card from '../../components/Card'
+import { View, Text, Image, ScrollView, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import InfoItem from '../../components/InfoItem';
+import Card from '../../components/Card';
+import { styles } from './styles';
 
 import icon1 from '../../assets/network.png';
 import icon2 from '../../assets/dev.png';
 import icon3 from '../../assets/box.png';
+import avatar from '../../assets/me.jpeg';
 
 export default function Home() {
     return (
         <SafeAreaView style={styles.container}>
-            <Card title="Software Development" img={icon1} />
-            <Card title="Web Development" img={icon2} />
-            <Card title="Web Design" img={icon3} />
+            <ScrollView>
+                <View style={styles.header}>
+                    <Text style={styles.textLeft}>Rafithy</Text>
+                    <View style={styles.contentRight}>
+                        <Text style={styles.contentRightText}>About me</Text>
+                        <Text style={styles.contentRightText}>Services</Text>
+                        <Text style={styles.contentRightText}>Contact me</Text>
+                    </View>
+                </View>
+                <View style={styles.presentation}>
+                    <View>
+                        <Text style={styles.title}>Hi, I am{`\n`}Rafithy Dev</Text>
+                        <Text style={styles.subtitle}>Fullstack Developer</Text>
+
+                        <View style={styles.presentationButtons}>
+                            <TouchableOpacity style={styles.downloadButton}>
+                                <Text style={styles.buttonText}>Download CV</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.learnMoreButton}>
+                                <Text style={styles.buttonText}>Learn More</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                    <Image style={styles.avatar} source={avatar} />
+                </View>
+
+                <Text style={styles.centeredTitle}>About</Text>
+
+                <Text style={styles.about}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </Text>
+
+                <View style={styles.info}>
+                    <InfoItem title="Full Name" subtitle='Rafithy' />
+                    <InfoItem title="Email Address" subtitle='rafithydev@gmail.com' />
+                    <InfoItem title="Website" subtitle='alvdelci.github.io' />
+                </View>
+
+                <Text style={styles.centeredTitle}>What i do</Text>
+
+                <View style={styles.whatIDo}>
+                    <Card title="Software Development" img={icon1} />
+                    <Card title="Web Development" img={icon2} />
+                    <Card title="Web Design" img={icon3} />
+                </View>
+
+                <Text style={styles.centeredTitle}>My Main Skills</Text>
+
+                <View style={styles.skills}>
+                    <View style={styles.skillItem}>
+                        <Text style={styles.skillPercent}>90%</Text>
+                        <Text style={styles.skillTitle}>REACT</Text>
+                    </View>
+                    <View style={styles.skillItem}>
+                        <Text style={styles.skillPercent}>80%</Text>
+                        <Text style={styles.skillTitle}>TYPESCRIPT</Text>
+                    </View>
+                    <View style={styles.skillItem}>
+                        <Text style={styles.skillPercent}>60%</Text>
+                        <Text style={styles.skillTitle}>NODE</Text>
+                    </View>
+                    <View style={styles.skillItem}>
+                        <Text style={styles.skillPercent}>30%</Text>
+                        <Text style={styles.skillTitle}>REACT NATIVE</Text>
+                    </View>
+                </View>
+
+                <Text style={styles.centeredTitle}>Get in touch</Text>
+                <Text style={styles.centeredTitle}>Get in touch</Text>
+
+            </ScrollView>
         </SafeAreaView>
     )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -4,9 +4,114 @@ import { THEME } from "../../theme";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row",
+    flexDirection: "column",
+    paddingTop: 10,
     backgroundColor: THEME.COLORS.BACKGROUND,
+  },
+  header: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  textLeft: {
+    fontWeight: "bold",
+    fontSize: 16,
+    color: THEME.COLORS.GRAY2,
+  },
+  contentRight: {
+    width: "60%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  contentRightText: {
+    color: THEME.COLORS.GRAY2,
+  },
+  presentation: {
+    width: "100%",
+    flexDirection: "row",
     justifyContent: "space-around",
     alignItems: "center",
+    marginTop: 30,
+  },
+  avatar: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: THEME.COLORS.GRAY2,
+  },
+  subtitle: {
+    fontSize: 11,
+    color: THEME.COLORS.GRAY2,
+  },
+  whatIDo: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  centeredTitle: {
+    fontWeight: "bold",
+    fontSize: 22,
+    alignSelf: "center",
+    marginTop: 15,
+    marginBottom: 15,
+    color: THEME.COLORS.GRAY2,
+  },
+  about: {
+    width: "85%",
+    alignSelf: "center",
+    textAlign: "center",
+    marginBottom: 15,
+    color: THEME.COLORS.GRAY2,
+  },
+  presentationButtons: {
+    flexDirection: "row",
+    marginTop: 10,
+  },
+  downloadButton: {
+    backgroundColor: THEME.COLORS.GREEN,
+    marginRight: 5,
+    padding: 6,
+  },
+  learnMoreButton: {
+    padding: 6,
+    borderWidth: 0.6,
+    backgroundColor: "transparent",
+    borderColor: "gray",
+  },
+  buttonText: {
+    fontSize: 11,
+    color: THEME.COLORS.GRAY2,
+  },
+  info: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginBottom: 20,
+  },
+  skills: {
+    flexDirection: "row",
+    width: "92%",
+    height: 100,
+    alignSelf: "center",
+    justifyContent: "space-around",
+    backgroundColor: THEME.COLORS.GRAY3,
+  },
+  skillItem: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  skillPercent: {
+    color: THEME.COLORS.GRAY1,
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  skillTitle: {
+    color: THEME.COLORS.GREEN,
+    fontSize: 11,
   },
 });

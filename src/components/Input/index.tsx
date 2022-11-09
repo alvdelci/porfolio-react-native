3e0b982f48f543a2ba8a80f24e9dcc23f04e2e97
alvdelci <rafithydev@gmail.com>
2022-11-09 16:04:20 -0300

    fix: Finalizando estilização

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,13 +1,32 @@
 import { View } from 'react-native';
 import { styles } from './styles';
 import { FloatingLabelInput } from 'react-native-floating-label-input';
+import { THEME } from '../../theme';
+import { useState } from 'react';
 
-export default function Input() {
+interface InputProps {
+    label: string;
+    mask?: string;
+}
+
+export default function Input(props: InputProps) {
+
+    const [value, setValue] = useState<string>("");
+
     return (
         <View style={styles.container}>
             <FloatingLabelInput
-                label='Name'
+                label={props.label}
                 containerStyles={styles.input}
+                customLabelStyles={{
+                    colorFocused: THEME.COLORS.GREEN,
+                    colorBlurred: THEME.COLORS.GRAY1,
+                    topFocused: -15,
+                    leftBlurred: -1,
+                }}
+                value={value}
+                onChangeText={value => setValue(value)}
+                mask={props.mask}
             />
         </View>
     );
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,12 +1,12 @@
 import { StyleSheet } from "react-native";
+import { THEME } from "../../theme";
 
 export const styles = StyleSheet.create({
   container: {
-    width: 100,
-    height: 50,
-    borderColor: "#fff",
+    width: 175,
+    height: 30,
+    borderBottomWidth: 0.5,
+    borderColor: THEME.COLORS.GRAY1,
   },
-  input: {
-    width: 100,
-  },
+  input: {},
 });
